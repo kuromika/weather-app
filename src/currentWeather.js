53252adbad107caa65c7e93437f9001df9eeed82
mika-alpha <yuukeikioku@gmail.com>
2022-09-28 18:01:25 -0500

    Add HTML elements

--- src/currentWeather.js
+++ src/currentWeather.js
@@ -2,15 +2,18 @@
 import { getCurrentData, getCoordinates} from "./openWeather"
 
 async function CurrentWeather(location) {
-    const data = await getCurrentData(location);
-    const coords = await getCoordinates(location);
+    const dataPromise = getCurrentData(location);
+    const coordsPromise = getCoordinates(location);
+    const [data, coords] = await Promise.all([dataPromise, coordsPromise]);
+
     const name = `${coords.name}, ${coords.country}`;
-    const { main: weather, description, icon } = data.weather[0];
+    const { main: weather, description} = data.weather[0];
     const { temp: temperature, feels_like: feels, pressure, humidity } = data.main;
     const { deg: windDegree, speed: windSpeed } = data.wind;
 
+
     return {
-        name, weather, description, icon,
+        name, weather, description,
         temperature, feels, pressure, humidity,
         windDegree, windSpeed
     }
