e71597bc03beb1bb3262a8284cab278763dacfe5
mika-alpha <yuukeikioku@gmail.com>
2022-09-28 23:23:32 -0500

    Fix api request not using https

--- src/openWeather.js
+++ src/openWeather.js
@@ -1,7 +1,7 @@
 
 const KEY = '38e5c0d2f94d0db0af3651a34cae725c';
 async function getCoordinates(name) {
-    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=5&appid=${KEY}`,{mode: "cors"});
+    const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=5&appid=${KEY}`,{mode: "cors"});
     const data = await response.json();
     return data[0];
 }
